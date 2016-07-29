0d887fbbbb713d9fa91dca2d176557774af98690
Rukaya Johaadien <JohaadienR@sanbi.local>
2016-07-29 09:17:05 +0200

    heroku collectstatic problems still

---
+++ website/static/leaflet-measure/README.md
@@ -0,0 +1,210 @@
+# leaflet-measure
+
+Coordinate, linear, and area measure control for [Leaflet](http://leafletjs.com) maps. Extends [L.Control](http://leafletjs.com/reference.html#control).
+
+<hr>
+
+## [Demo](http://ljagis.github.io/leaflet-measure)
+
+[![Demo](https://raw.githubusercontent.com/ljagis/leaflet-measure/master/example/leaflet-measure-demo.jpg)](http://ljagis.github.io/leaflet-measure)
+
+<hr>
+
+## Install Options
+
+- Clone.. `git clone https://github.com/ljagis/leaflet-measure.git`
+
+- Install with [Bower](http://bower.io/).. `bower install leaflet-measure`
+
+- Install with [npm](https://www.npmjs.com/).. `npm install leaflet-measure`
+
+## Use bundled `leaflet-measure.min.js` or `leaflet-measure.js`
+
+Include the [Leaflet Source](http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js), `leaflet-measure.min.js`, and associated CSS stylesheets in your HTML page
+
+```html
+<!doctype HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css">
+  <link rel="stylesheet" href="leaflet-measure.css">
+</head>
+<body>
+  <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+  <script src="leaflet-measure.min.js"></script>
+  <script>
+    // Start creating maps
+  </script>
+</body>
+</html>
+```
+
+## Use with [npm](https://www.npmjs.com/)
+
+```javascript
+var L = require('leaflet');
+require('leaflet-measure');
+
+// Start creating maps
+```
+
+<hr>
+
+## Add control to a Leaflet map
+
+`leaflet-measure` adds `L.Control.Measure`. This control may be used with the standard Leaflet control workflows [described in the Leaflet docs](http://leafletjs.com/reference.html#control).
+
+The measure control can be instantiated directly and added to a map:
+```javascript
+var myMap = L.map('mapElementId', options);
+var measureControl = new L.Control.Measure(options);
+measureControl.addTo(myMap);
+```
+
+or instantiated via the factory:
+```javascript
+var myMap = L.map('mapElementId', options);
+var measureControl = L.control.measure(options);
+measureControl.addTo(myMap);
+```
+
+or added to a map using map options:
+```javascript
+var myMap = L.map('mapElementId', {
+  measureControl: true
+});
+```
+
+<hr>
+
+## Control options
+
+### position
+
+`{ position: 'topright' }`
+
+Standard Leaflet control [position options](http://leafletjs.com/reference.html#control-positions)
+
+### primaryLengthUnit | secondaryLengthUnit
+
+`{ primaryLengthUnit: 'feet', secondaryLengthUnit: 'miles' }`
+
+Units used to display length results. `secondaryLengthUnit` is optional.
+
+Valid values are `feet`, `meters`, `miles`, and `kilometers`
+
+### primaryAreaUnit | secondaryAreaUnit
+
+`{ primaryAreaUnit: 'acres', secondaryAreaUnit: undefined }`
+
+Units used to display area results. `secondaryAreaUnit` is optional.
+
+Valid values are `acres`, `hectares`, `sqfeet`, `sqmeters`, and `sqmiles`
+
+### activeColor
+
+`{ activeColor: '#ABE67E' }`
+
+Base color to use for map features rendered while actively performing a measurement. Value should be a color represented as a hexadecimal string.
+
+### completedColor
+
+`{ completedColor: '#C8F2BE' }`
+
+Base color to use for features generated from a completed measurement. Value should be a color represented as a hexadecimal string.
+
+### popupOptions
+
+`popupOptions: { className: 'leaflet-measure-resultpopup', autoPanPadding: [10, 10] }`
+
+Options applied to the popup of the resulting measure feature. Properties may be any standard Leaflet [popup options](http://leafletjs.com/reference.html#popup-options).
+
+### units
+
+Custom units to make available to the measurement calculator. Packaged units are `feet`, `meters`, `miles`, and `kilometers` for length and `acres`, `hectares`, `sqfeet`, `sqmeters`, and `sqmiles` for areas. Additional unit definitions can be added to the packaged units using this option.
+
+Define units as
+
+```javascript
+{
+  someNewUnit: {
+    factor: 0.001, // Required. Factor to apply when converting to this unit. Length in meters or area in sq meters will be multiplied by this factor.
+    display: 'My New Unit', // Required. How to display in results, like.. "300 Meters (0.3 My New Unit)".
+    decimals: 2 // Number of decimals to round results when using this unit. `0` is the default value if not specified.
+  },
+  myOtherNewUnit: {
+    factor: 1234,
+    display: 'My Other Unit',
+    decimals: 0
+  }
+}
+```
+
+### captureZIndex
+
+`{ captureZIndex: 10000 }`
+
+Z-index of the marker used to capture measure clicks. Set this value higher than the z-index of all other map layers to disable click events on other layers while a measurement is active.
+
+### localization
+
+`{ localization: 'en' }`
+
+Locale to translate displayed text.
+
+Available locales include `en` (default), `es`, `fr`, `it`, `nl`, and `ru`.
+
+### decPoint | thousandsSep
+
+`{ decPoint: '.', thousandsSep: ',' }`
+
+Decimal point and thousands separator used when displaying measurements. If not specified, values are defined by the localization.
+
+<hr>
+
+## Events
+
+You can subscribe to the following events on the [Map](http://leafletjs.com/reference.html#map-class) using [these methods](http://leafletjs.com/reference.html#events)
+
+### measurestart
+
+Fired when measurement starts
+
+### measurefinish
+
+Fired when measurement finishes with results of the measurement. Results data includes:
+
+- `area`: Area of a polygon measurement in sq meters. 0 for measurements with less than 3 points.
+
+- `areaDisplay`: Area formatted as displayed in the popup.
+
+- `lastCoord`: Last point clicked in both decimal degrees and degress/min/seconds.
+
+- `length`: Length of the measurement in meters. 0 for measurements with less than 2 points.
+
+- `lengthDisplay`: Length formatted as displayed in the popup.
+
+- `pointCount`: Number of points directly added by the user.
+
+- `points`: Array of points as [`LatLng`](http://leafletjs.com/reference.html#latlng) used to calculate the measurement. Number of items in the array may differ from `pointCoint` because an additional point is added to close polygons during polygon measurements.
+
+<hr>
+
+## Customizing map feature styles
+
+Map features may be styled using CSS SVG style attributes. Features generated from `leaflet-measure` measurements are given the following class names:
+
+- **layer-measurearea**: Feature displaying area of an active measurement
+
+- **layer-measureboundary**: Feature displaying the linear path of an active measurement
+
+- **layer-measurevertex**: Feature added at each vertex (measurement click) of an active measurement
+
+- **layer-measuredrag**: Symbol following cursor while moving during an active measurement
+
+- **layer-measure-resultarea**: Feature added to the map as a permanent layer resulting from an area (3+ points) measurement
+
+- **layer-measure-resultline**: Feature added to the map as a permanent layer resulting from a linear (2 point) measurement
+
+- **layer-measure-resultpoint**: Featured added to the map as a permanent layer resulting from a point (single click) measurement
---
+++ website/static/leaflet-measure/src/leaflet-measure.js
@@ -0,0 +1,447 @@
+// leaflet-measure.js
+
+var _ = require('underscore');
+var L = require('leaflet');
+var humanize = require('humanize');
+
+var units = require('./units');
+var calc = require('./calc');
+var dom = require('./dom');
+var $ = dom.$;
+
+var Symbology = require('./mapsymbology');
+
+var fs = require('fs');
+var controlTemplate = _.template(fs.readFileSync(__dirname + '/leaflet-measure-template.html', 'utf8'));
+var resultsTemplate = _.template(fs.readFileSync(__dirname + '/leaflet-measure-template-results.html', 'utf8'));
+var pointPopupTemplate = _.template(fs.readFileSync(__dirname + '/popuptemplates/point-popuptemplate.html', 'utf8'));
+var linePopupTemplate = _.template(fs.readFileSync(__dirname + '/popuptemplates/line-popuptemplate.html', 'utf8'));
+var areaPopupTemplate = _.template(fs.readFileSync(__dirname + '/popuptemplates/area-popuptemplate.html', 'utf8'));
+
+var i18n = new (require('i18n-2'))({
+  devMode: false,
+  locales: {
+    'de': require('./i18n/de'),
+    'en': require('./i18n/en'),
+    'es': require('./i18n/es'),
+    'fr': require('./i18n/fr'),
+    'it': require('./i18n/it'),
+    'nl': require('./i18n/nl'),
+    'pt_BR': require('./i18n/pt_BR'),
+    'ru': require('./i18n/ru'),
+    'tr': require('./i18n/tr')
+  }
+});
+
+L.Control.Measure = L.Control.extend({
+  _className: 'leaflet-control-measure',
+  options: {
+    units: {},
+    position: 'topright',
+    primaryLengthUnit: 'feet',
+    secondaryLengthUnit: 'miles',
+    primaryAreaUnit: 'acres',
+    activeColor: '#ABE67E',     // base color for map features while actively measuring
+    completedColor: '#C8F2BE',  // base color for permenant features generated from completed measure
+    captureZIndex: 10000,       // z-index of the marker used to capture measure events
+    popupOptions: {             // standard leaflet popup options http://leafletjs.com/reference.html#popup-options
+      className: 'leaflet-measure-resultpopup',
+      autoPanPadding: [10, 10]
+    }
+  },
+  initialize: function (options) {
+    L.setOptions(this, options);
+    this.options.units = L.extend({}, units, this.options.units);
+    this._symbols = new Symbology(_.pick(this.options, 'activeColor', 'completedColor'));
+    i18n.setLocale(this.options.localization);
+  },
+  onAdd: function (map) {
+    this._map = map;
+    this._latlngs = [];
+    this._initLayout();
+    map.on('click', this._collapse, this);
+    this._layer = L.layerGroup().addTo(map);
+    return this._container;
+  },
+  onRemove: function (map) {
+    map.off('click', this._collapse, this);
+    map.removeLayer(this._layer);
+  },
+  _initLayout: function () {
+    var className = this._className, container = this._container = L.DomUtil.create('div', className);
+    var $toggle, $start, $cancel, $finish;
+
+    container.innerHTML = controlTemplate({
+      model: {
+        className: className
+      },
+      i18n: i18n
+    });
+
+    // copied from leaflet
+    // https://bitbucket.org/ljagis/js-mapbootstrap/src/4ab1e9e896c08bdbc8164d4053b2f945143f4f3a/app/components/measure/leaflet-measure-control.js?at=master#cl-30
+    container.setAttribute('aria-haspopup', true);
+    if (!L.Browser.touch) {
+      L.DomEvent.disableClickPropagation(container);
+      L.DomEvent.disableScrollPropagation(container);
+    } else {
+      L.DomEvent.on(container, 'click', L.DomEvent.stopPropagation);
+    }
+
+    $toggle = this.$toggle = $('.js-toggle', container);         // collapsed content
+    this.$interaction = $('.js-interaction', container);         // expanded content
+    $start = $('.js-start', container);                          // start button
+    $cancel = $('.js-cancel', container);                        // cancel button
+    $finish = $('.js-finish', container);                        // finish button
+    this.$startPrompt = $('.js-startprompt', container);         // full area with button to start measurment
+    this.$measuringPrompt = $('.js-measuringprompt', container); // full area with all stuff for active measurement
+    this.$startHelp = $('.js-starthelp', container);             // "Start creating a measurement by adding points"
+    this.$results = $('.js-results', container);                 // div with coordinate, linear, area results
+    this.$measureTasks = $('.js-measuretasks', container);       // active measure buttons container
+
+    this._collapse();
+    this._updateMeasureNotStarted();
+
+    if (!L.Browser.android) {
+      L.DomEvent.on(container, 'mouseenter', this._expand, this);
+      L.DomEvent.on(container, 'mouseleave', this._collapse, this);
+    }
+    L.DomEvent.on($toggle, 'click', L.DomEvent.stop);
+    if (L.Browser.touch) {
+      L.DomEvent.on($toggle, 'click', this._expand, this);
+    } else {
+      L.DomEvent.on($toggle, 'focus', this._expand, this);
+    }
+    L.DomEvent.on($start, 'click', L.DomEvent.stop);
+    L.DomEvent.on($start, 'click', this._startMeasure, this);
+    L.DomEvent.on($cancel, 'click', L.DomEvent.stop);
+    L.DomEvent.on($cancel, 'click', this._finishMeasure, this);
+    L.DomEvent.on($finish, 'click', L.DomEvent.stop);
+    L.DomEvent.on($finish, 'click', this._handleMeasureDoubleClick, this);
+  },
+  _expand: function () {
+    dom.hide(this.$toggle);
+    dom.show(this.$interaction);
+  },
+  _collapse: function () {
+    if (!this._locked) {
+      dom.hide(this.$interaction);
+      dom.show(this.$toggle);
+    }
+  },
+  // move between basic states:
+  // measure not started, started/in progress but no points added, in progress and with points
+  _updateMeasureNotStarted: function () {
+    dom.hide(this.$startHelp);
+    dom.hide(this.$results);
+    dom.hide(this.$measureTasks);
+    dom.hide(this.$measuringPrompt);
+    dom.show(this.$startPrompt);
+  },
+  _updateMeasureStartedNoPoints: function () {
+    dom.hide(this.$results);
+    dom.show(this.$startHelp);
+    dom.show(this.$measureTasks);
+    dom.hide(this.$startPrompt);
+    dom.show(this.$measuringPrompt);
+  },
+  _updateMeasureStartedWithPoints: function () {
+    dom.hide(this.$startHelp);
+    dom.show(this.$results);
+    dom.show(this.$measureTasks);
+    dom.hide(this.$startPrompt);
+    dom.show(this.$measuringPrompt);
+  },
+  // get state vars and interface ready for measure
+  _startMeasure: function () {
+    this._locked = true;
+    this._measureVertexes = L.featureGroup().addTo(this._layer);
+    this._captureMarker = L.marker(this._map.getCenter(), {
+      clickable: true,
+      zIndexOffset: this.options.captureZIndex,
+      opacity: 0
+    }).addTo(this._layer);
+    this._setCaptureMarkerIcon();
+
+    this._captureMarker
+      .on('mouseout', this._handleMapMouseOut, this)
+      .on('dblclick', this._handleMeasureDoubleClick, this)
+      .on('click', this._handleMeasureClick, this);
+
+    this._map
+      .on('mousemove', this._handleMeasureMove, this)
+      .on('mouseout', this._handleMapMouseOut, this)
+      .on('move', this._centerCaptureMarker, this)
+      .on('resize', this._setCaptureMarkerIcon, this);
+
+    L.DomEvent.on(this._container, 'mouseenter', this._handleMapMouseOut, this);
+
+    this._updateMeasureStartedNoPoints();
+
+    this._map.fire('measurestart', null, false);
+  },
+  // return to state with no measure in progress, undo `this._startMeasure`
+  _finishMeasure: function () {
+    var model = _.extend({}, this._resultsModel, {
+      points: this._latlngs
+    });
+
+    this._locked = false;
+
+    L.DomEvent.off(this._container, 'mouseover', this._handleMapMouseOut, this);
+
+    this._clearMeasure();
+
+    this._captureMarker
+      .off('mouseout', this._handleMapMouseOut, this)
+      .off('dblclick', this._handleMeasureDoubleClick, this)
+      .off('click', this._handleMeasureClick, this);
+
+    this._map
+      .off('mousemove', this._handleMeasureMove, this)
+      .off('mouseout', this._handleMapMouseOut, this)
+      .off('move', this._centerCaptureMarker, this)
+      .off('resize', this._setCaptureMarkerIcon, this);
+
+    this._layer
+      .removeLayer(this._measureVertexes)
+      .removeLayer(this._captureMarker);
+    this._measureVertexes = null;
+
+    this._updateMeasureNotStarted();
+    this._collapse();
+
+    this._map.fire('measurefinish', model, false);
+  },
+  // clear all running measure data
+  _clearMeasure: function () {
+    this._latlngs = [];
+    this._resultsModel = null;
+    this._measureVertexes.clearLayers();
+    if (this._measureDrag) {
+      this._layer.removeLayer(this._measureDrag);
+    }
+    if (this._measureArea) {
+      this._layer.removeLayer(this._measureArea);
+    }
+    if (this._measureBoundary) {
+      this._layer.removeLayer(this._measureBoundary);
+    }
+    this._measureDrag = null;
+    this._measureArea = null;
+    this._measureBoundary = null;
+  },
+  // centers the event capture marker
+  _centerCaptureMarker: function () {
+    this._captureMarker.setLatLng(this._map.getCenter());
+  },
+  // set icon on the capture marker
+  _setCaptureMarkerIcon: function () {
+    this._captureMarker.setIcon(L.divIcon({
+      iconSize: this._map.getSize().multiplyBy(2)
+    }));
+  },
+  // format measurements to nice display string based on units in options
+  // `{ lengthDisplay: '100 Feet (0.02 Miles)', areaDisplay: ... }`
+  _getMeasurementDisplayStrings: function (measurement) {
+    var unitDefinitions = this.options.units;
+
+    return {
+      lengthDisplay: buildDisplay(measurement.length, this.options.primaryLengthUnit, this.options.secondaryLengthUnit, this.options.decPoint, this.options.thousandsSep),
+      areaDisplay: buildDisplay(measurement.area, this.options.primaryAreaUnit, this.options.secondaryAreaUnit, this.options.decPoint, this.options.thousandsSep)
+    };
+
+    function buildDisplay (val, primaryUnit, secondaryUnit, decPoint, thousandsSep) {
+      var display;
+      if (primaryUnit && unitDefinitions[primaryUnit]) {
+        display = formatMeasure(val, unitDefinitions[primaryUnit], decPoint, thousandsSep);
+        if (secondaryUnit && unitDefinitions[secondaryUnit]) {
+          display = display + ' (' +  formatMeasure(val, unitDefinitions[secondaryUnit], decPoint, thousandsSep) + ')';
+        }
+      } else {
+        display = formatMeasure(val, null, decPoint, thousandsSep);
+      }
+      return display;
+    }
+
+    function formatMeasure (val, unit, decPoint, thousandsSep) {
+      return unit && unit.factor && unit.display ?
+        humanize.numberFormat(val * unit.factor, unit.decimals || 0, decPoint || i18n.__('decPoint'), thousandsSep || i18n.__('thousandsSep')) + ' ' + i18n.__([unit.display]) || unit.display :
+        humanize.numberFormat(val, 0, decPoint || i18n.__('decPoint'), thousandsSep || i18n.__('thousandsSep'));
+    }
+  },
+  // update results area of dom with calced measure from `this._latlngs`
+  _updateResults: function () {
+    var calced = calc.measure(this._latlngs);
+    var resultsModel = this._resultsModel = _.extend({}, calced, this._getMeasurementDisplayStrings(calced), {
+      pointCount: this._latlngs.length
+    });
+    this.$results.innerHTML = resultsTemplate({
+      model: resultsModel,
+      humanize: humanize,
+      i18n: i18n
+    });
+  },
+  // mouse move handler while measure in progress
+  // adds floating measure marker under cursor
+  _handleMeasureMove: function (evt) {
+    if (!this._measureDrag) {
+      this._measureDrag = L.circleMarker(evt.latlng, this._symbols.getSymbol('measureDrag')).addTo(this._layer);
+    } else {
+      this._measureDrag.setLatLng(evt.latlng);
+    }
+    this._measureDrag.bringToFront();
+  },
+  // handler for both double click and clicking finish button
+  // do final calc and finish out current measure, clear dom and internal state, add permanent map features
+  _handleMeasureDoubleClick: function () {
+    var latlngs = this._latlngs, calced, resultFeature, popupContainer, popupContent, zoomLink, deleteLink;
+
+    this._finishMeasure();
+
+    if (!latlngs.length) {
+      return;
+    }
+
+    if (latlngs.length > 2) {
+      latlngs.push(_.first(latlngs)); // close path to get full perimeter measurement for areas
+    }
+
+    calced = calc.measure(latlngs);
+
+    if (latlngs.length === 1) {
+      resultFeature = L.circleMarker(latlngs[0], this._symbols.getSymbol('resultPoint'));
+      popupContent = pointPopupTemplate({
+        model: calced,
+        humanize: humanize,
+        i18n: i18n
+      });
+    } else if (latlngs.length === 2) {
+      resultFeature = L.polyline(latlngs, this._symbols.getSymbol('resultLine'));
+      popupContent = linePopupTemplate({
+        model: _.extend({}, calced, this._getMeasurementDisplayStrings(calced)),
+        humanize: humanize,
+        i18n: i18n
+      });
+    } else {
+      resultFeature = L.polygon(latlngs, this._symbols.getSymbol('resultArea'));
+      popupContent = areaPopupTemplate({
+        model: _.extend({}, calced, this._getMeasurementDisplayStrings(calced)),
+        humanize: humanize,
+        i18n: i18n
+      });
+    }
+
+    popupContainer = L.DomUtil.create('div', '');
+    popupContainer.innerHTML = popupContent;
+
+    zoomLink = $('.js-zoomto', popupContainer);
+    if (zoomLink) {
+      L.DomEvent.on(zoomLink, 'click', L.DomEvent.stop);
+      L.DomEvent.on(zoomLink, 'click', function () {
+        this._map.fitBounds(resultFeature.getBounds(), {
+          padding: [20, 20],
+          maxZoom: 17
+        });
+      }, this);
+    }
+
+    deleteLink = $('.js-deletemarkup', popupContainer);
+    if (deleteLink) {
+      L.DomEvent.on(deleteLink, 'click', L.DomEvent.stop);
+      L.DomEvent.on(deleteLink, 'click', function () {
+        // TODO. maybe remove any event handlers on zoom and delete buttons?
+        this._layer.removeLayer(resultFeature);
+      }, this);
+    }
+
+    resultFeature.addTo(this._layer);
+    resultFeature.bindPopup(popupContainer, this.options.popupOptions);
+    resultFeature.openPopup(resultFeature.getBounds().getCenter());
+  },
+  // handle map click during ongoing measurement
+  // add new clicked point, update measure layers and results ui
+  _handleMeasureClick: function (evt) {
+    var latlng = this._map.mouseEventToLatLng(evt.originalEvent), // get actual latlng instead of the marker's latlng from originalEvent
+      lastClick = _.last(this._latlngs),
+      vertexSymbol = this._symbols.getSymbol('measureVertex');
+
+    if (!lastClick || !latlng.equals(lastClick)) { // skip if same point as last click, happens on `dblclick`
+      this._latlngs.push(latlng);
+      this._addMeasureArea(this._latlngs);
+      this._addMeasureBoundary(this._latlngs);
+
+      this._measureVertexes.eachLayer(function (layer) {
+        layer.setStyle(vertexSymbol);
+        // reset all vertexes to non-active class - only last vertex is active
+        // `layer.setStyle({ className: 'layer-measurevertex'})` doesn't work. https://github.com/leaflet/leaflet/issues/2662
+        // set attribute on path directly
+        layer._path.setAttribute('class', vertexSymbol.className);
+      });
+
+      this._addNewVertex(latlng);
+
+      if (this._measureBoundary) {
+        this._measureBoundary.bringToFront();
+      }
+      this._measureVertexes.bringToFront();
+    }
+
+    this._updateResults();
+    this._updateMeasureStartedWithPoints();
+  },
+  // handle map mouse out during ongoing measure
+  // remove floating cursor vertex from map
+  _handleMapMouseOut: function () {
+    if (this._measureDrag) {
+      this._layer.removeLayer(this._measureDrag);
+      this._measureDrag = null;
+    }
+  },
+  // add various measure graphics to map - vertex, area, boundary
+  _addNewVertex: function (latlng) {
+    L.circleMarker(latlng, this._symbols.getSymbol('measureVertexActive')).addTo(this._measureVertexes);
+  },
+  _addMeasureArea: function (latlngs) {
+    if (latlngs.length < 3) {
+      if (this._measureArea) {
+        this._layer.removeLayer(this._measureArea);
+        this._measureArea = null;
+      }
+      return;
+    }
+    if (!this._measureArea) {
+      this._measureArea = L.polygon(latlngs, this._symbols.getSymbol('measureArea')).addTo(this._layer);
+    } else {
+      this._measureArea.setLatLngs(latlngs);
+    }
+  },
+  _addMeasureBoundary: function (latlngs) {
+    if (latlngs.length < 2) {
+      if (this._measureBoundary) {
+        this._layer.removeLayer(this._measureBoundary);
+        this._measureBoundary = null;
+      }
+      return;
+    }
+    if (!this._measureBoundary) {
+      this._measureBoundary = L.polyline(latlngs, this._symbols.getSymbol('measureBoundary')).addTo(this._layer);
+    } else {
+      this._measureBoundary.setLatLngs(latlngs);
+    }
+  }
+});
+
+L.Map.mergeOptions({
+  measureControl: false
+});
+
+L.Map.addInitHook(function () {
+  if (this.options.measureControl) {
+    this.measureControl = (new L.Control.Measure()).addTo(this);
+  }
+});
+
+L.control.measure = function (options) {
+  return new L.Control.Measure(options);
+};
